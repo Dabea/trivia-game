d4961245432bf568f40100c9020ffff90879637f
Abraham <abeis@msn.com>
2018-03-15 23:47:40 -0500

    changed white spcae

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -1,12 +1,21 @@
 
-
+ let timer = 30;
 const countDownTimer = function(time){
-    let timer = time;
+   timer = time;
     setInterval(function(){
         timer--;
-       document.getElementsByClassName('timer2').innerHTML = `Timer ${timer}`;
-       console.log('1');
+       document.getElementsByClassName('timer2')[0].textContent = `${timer}`;
+       if(timer <= 0){
+            test();
+       }
     }, 1000);
 }
+ if(timer === 0){
+     alert('Sorry Wrong answer')
+ }
+function test(){
+    alert('im done');
+}
 
-countDownTimer(30);
+countDownTimer(3);
+
