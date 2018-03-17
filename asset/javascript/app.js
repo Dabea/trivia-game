5d62cc52d38d0f9ce77cd093421f58b4c7fee96d
Abraham <abeis@msn.com>
2018-03-16 23:43:36 -0500

    defined basic logic

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -6,16 +6,83 @@ const countDownTimer = function(time){
         timer--;
        document.getElementsByClassName('timer2')[0].textContent = `${timer}`;
        if(timer <= 0){
+           timer = 30;
             test();
        }
     }, 1000);
 }
+
+
+let count = -1;
+const questionCollection = [
+    {"question": "In the song &ldquo;The Ultimate Showdown of Ultimate Destiny,&rdquo; who is the only one to survive the battle?","correct_answer":"Mr. Rogers" ,"incorrect_answers":["Batman","Chuck Norris","Godzilla"]},
+    {"question":"Vatican City is a country.","correct_answer":"True","incorrect_answers":["False"]},{"question":"Which of the following ancient Near Eastern peoples still exists as a modern ethnic group?","correct_answer":"Assyrians","incorrect_answers":["Babylonians","Hittites","Elamites"]},
+    {"question":"Who was the first female protagonist in a video game?","correct_answer":"Samus Aran","incorrect_answers":["Lara Croft","Alis Landale","Chell"]},
+    {"question":"Which Overwatch character says the line &quot;Heroes never die!&quot;?","correct_answer":"Mercy","incorrect_answers":["Reaper","Sonic","Ana"]},
+    {"question":"What is lost in Hawaiian and is also the name of a little girl in a 2002 film which features a alien named &quot;Stitch&quot;?","correct_answer":"Lilo","incorrect_answers":["Lolo","Lucy","Lulu"]},
+    {"question":"Who created Ultron of Earth-616?","correct_answer":"Henry Pym","incorrect_answers":["Amadeus Cho","Tony Stark","Reed Richards"]},
+    {"question":"Who was the winner of the 2016 WWE Royal Rumble?","correct_answer":"Triple H","incorrect_answers":["Roman Reigns","AJ Styles","Dean Ambrose"]},
+    {"question":"What year was Min Yoongi from South Korea boy band &quot;BTS&quot; born in?","correct_answer":"1993","incorrect_answers":["1992","1995","1994"]},
+    {"question":"Which ones of these Mario Kart games was made for the Gameboy Advance?","correct_answer":"Mario Kart: Super Circuit","incorrect_answers":["Mario Kart: Double Dash","Mario Kart 64","Super Mario Kart"]}
+];
+
+function generateQuestionTemplate(question){
+    questionOptions = question.incorrect_answers;
+    questionOptions.push(question.correct_answer);
+    const questionTemplate = `
+    <div class="question">
+    ${question.question}
+    </div>
+    <div class="answers">
+        <ul>
+           ${questionOptions.map(answer => `<li><button value="${answer}" class="btn">${answer}</button></li>`).join(' ')}
+        </ul>
+    </div>`;
+    document.getElementsByClassName('question-area')[0].innerHTML =questionTemplate ;
+    enable();
+}
+
+function randomizeAnswersOrder(question){
+    
+   
+
+    return question
+}
+
  if(timer === 0){
      alert('Sorry Wrong answer')
  }
 function test(){
-    alert('im done');
+
+    if(count < questionCollection.length){
+        count++;
+        generateQuestionTemplate(questionCollection[count]);
+        
+    }else{
+        alert('game Over');
+    }
+  
+}
+function enable (){
+
+    $('.btn').on('click',  function(event){
+        if($(event.target).val() ===  questionCollection[count].correct_answer){
+            console.log('correct');
+        }else{
+            console.log('WRONG!');
+        }
+    } );
 }
 
-countDownTimer(3);
+
+function init(){
+    countDownTimer(3);
+
+    enable();
+}
+
+$( document ).ready(function(){
+    init();
+})
+
 
